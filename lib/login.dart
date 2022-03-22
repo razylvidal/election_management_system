cad3dd0fef72b9f5c8c4edea6c58bb8fec1fafb4
razylvidal <razylabbygail.vidal@unc.edu.ph>
2022-03-22 19:37:03 +0800

    New Login page design

--- lib/login.dart
+++ lib/login.dart
@@ -1,82 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class LoginPage extends StatelessWidget {
 
+ 
+
   @override
+  
   Widget build(BuildContext context) {
     
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget> [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  width: 1000,
-                  height: 600,
-                  decoration: BoxDecoration(  
-                  border: Border.all(color: Colors.grey, width: 2),  
-                  borderRadius: BorderRadius.circular(10), 
-                  color: Colors.grey[400],
-                  boxShadow: const [  
-                        BoxShadow(
-                        color: Colors.black87,
-                        offset: Offset(6.0, 6.0),
-                        blurRadius: 10.0,
-                      ),  
-                    ],
-                  //for background image
-                   image: const DecorationImage(
-                      image: AssetImage(
-                       'assets/backgroundImage.png'
-                      ),
-                      fit: BoxFit.fill,
-                   )
-                  ),
-                  //login details
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 10.0, top: 10.0),
-                    child: MyStatefulWidget(),
+      
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF8A2387),
+                  Color(0xFFE94057),
+                  Color(0xFFF27121),
+                ] 
                 )
-              
-              )
 
-              ),
-              
-          ]
-        
-        ),
-       
-      )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget> [
+                SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(padding: EdgeInsets.only(top: 25.0),
+                        child: Image.asset('assets/logo.png')
+                    ),
+                ),
+                ),
+                
+                const Text(
+                  'Election Management System',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  height: 400,
+                  width: 600,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 30),
+                      Text(
+                        'asdfghjkl',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Users(),
+                      )
+                      
+                    ],
+                  ),
+                )
+    
+              ],
+            ),
+
+
+          )
+        ,),
      
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({ Key? key }) : super(key: key);
-
+class Users extends StatefulWidget {
+  const Users({ Key? key }) : super(key: key);
+  
   @override
-  State<MyStatefulWidget> createState() => Logo();
+  State<Users> createState() => _UsersState();
 }
 
-class Logo extends State<MyStatefulWidget> {
+class _UsersState extends State<Users> {
+   String selectedValue = 'Login As';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          width: 80,
-          height: 80,
-         child: Image.asset(
-           'assets/logo.png'
-           
-          ),
-        
-        ),
-       
-      ],
+    return DropdownButton(
+                        value: selectedValue,
+                        items: const [
+                          DropdownMenuItem(
+                            child: Text('Admin'),
+                              value: 'Admin',
+                            ),
+                            DropdownMenuItem(
+                            child: Text('Voter'),
+                              value: 'Voter',
+                            ),
+                            DropdownMenuItem(
+                            child: Text('Campaign Manager'),
+                              value: 'Campaign Manager',
+                            ),
+                        ],
+                      
+                        onChanged: (value) {
+                          setState(() {
+                          selectedValue = 'its a prank';
+                          }
+                         );
+                        }
     );
   }
 }
