b1ab06335f13d8589c54b5207aef3c21a2e9751b
JvAborde1 <85803750+Psycho766@users.noreply.github.com>
2022-04-06 01:36:34 +0800

    Update Homepage

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -14,24 +14,18 @@ class HomePage extends StatelessWidget {
 
       //Top Navigation Bar
       appBar: topNavigationBar(context, scaffoldKey),
-      
-      
 
       //hamburger menu
-      drawer: Drawer(
+      endDrawer: Drawer(
         child: ListView(
-          padding: const EdgeInsets.only(left: 0,right: 0),
+          padding: EdgeInsets.zero,
           children: <Widget>[
             const DrawerHeader(
               decoration: BoxDecoration(
                 color: Color(0xFF091C32),
               ),
-<<<<<<< Updated upstream
-
 
               //Navigation Label
-=======
->>>>>>> Stashed changes
               child: Text(
                 'Dashboard',
                 style: TextStyle(
@@ -39,8 +33,8 @@ class HomePage extends StatelessWidget {
                   fontSize: 24,
                 ),
               ),
+            ),
 
-<<<<<<< Updated upstream
 
             //Label Manage
             const ListTile(
@@ -134,117 +128,12 @@ class HomePage extends StatelessWidget {
               ),
               padding: const EdgeInsets.only(top: 25, bottom: 25),
             ),
-=======
-              //Navigation Label
-              
-            ),
-
-          //Dashboard
-                ListTile(
-                  leading: const Icon(Icons.dashboard),
-                  title: const CustomText(
-                    text: "Dashboard",
-                    size: 20,
-                    color: Colors.black,
-                    weight: FontWeight.w700,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(),
-                      ),
-                    );
-                  },
-                ),
-
-                //Election
-                ListTile(
-                  leading: const Icon(Icons.assessment),
-                  title: const CustomText(
-                    text: "Elections",
-                    size: 20,
-                    color: Colors.black,
-                    weight: FontWeight.w700,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ElectionPage(),
-                      ),
-                    );
-                  },
-                ),
-
-                //Candidates
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: const CustomText(
-                    text: "Candidates",
-                    size: 20,
-                    color: Colors.black,
-                    weight: FontWeight.w700,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CandidatePage(),
-                      ),
-                    );
-                  },
-                ),
-
-                //Voters
-                ListTile(
-                  leading: const Icon(Icons.people),
-                  title: const CustomText(
-                    text: "Voters",
-                    size: 20,
-                    color: Colors.black,
-                    weight: FontWeight.w700,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VotersPage(),
-                      ),
-                    );
-                  },
-                ),
-
-                //settings
-                ListTile(
-                  leading: const Icon(Icons.settings),
-                  title: const CustomText(
-                    text: "Settings",
-                    size: 20,
-                    color: Colors.black,
-                    weight: FontWeight.w700,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VotersPage(),
-                      ),
-                    );
-                  },
-                ),
-
-
-                
-
->>>>>>> Stashed changes
           ],
         ),
       ),
 
 
       //Body
-<<<<<<< Updated upstream
       body: Container(
         child: Column(
           children: <Widget>[
@@ -280,12 +169,6 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-=======
-      body: Column(
-        
-      )
-
->>>>>>> Stashed changes
 
 
 
